deecd9050c009e195b5ca21fd3984505a6523f92
Anuj Kumar Mishra <144154519+iamakmishra11@users.noreply.github.com>
2025-02-07 02:00:26 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+// import React from "react";
 import Sidebar from "./components/Sidebar";
 import Header from "./components/Header";
 import ProductTable from "./components/ProductTable";
